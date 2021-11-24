99e774296153adda6a0264cebf10d6a57e6fffe4
Artem Zakharenko <zakhart1234567890@gmail.com>
2021-11-24 19:41:15 +0300

    Add two threads deadlock test

---
+++ tests/deadlock-test04/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+void no_opearation() {
+	int x = 0;
+}
+
+int main() {
+	std::mutex m1;
+	std::mutex m2;
+	no_opearation(); //main started
+	std::thread t1([&]() {
+		std::unique_lock<std::mutex> l1(m1);
+		no_opearation(); //thread started working
+		std::unique_lock<std::mutex> l2(m2);
+	});
+	std::thread t2([&]() {
+		std::unique_lock<std::mutex> l1(m2);
+		no_opearation(); //thread started working
+		std::unique_lock<std::mutex> l2(m1);
+	});
+
+	t1.join();
+	return 0;
+}
---
+++ tests/deadlock-test04/test.py
@@ -0,0 +1,36 @@
+import lldb
+import deadlock_detector
+import test_tools
+
+
+class TestTwoThreadsDeadlockClang(test_tools.MySetUpTestCase):
+    def setUp(self) -> None:
+        self.mySetUp(["main started", "thread started working"], "clang++")
+
+    def test_run(self) -> None:
+        error = self.launch_process()
+        self.continue_to_threads_beginning(2)
+        self.continue_process_n_sec(1)
+        has_deadlock, deadlock_cycle = deadlock_detector.find_deadlock(self.debugger)
+        self.assertTrue(has_deadlock)
+        deadlock_node_cycle = [x[0] for x in test_tools.GraphTesting.shift_cycle(deadlock_cycle, 2)]
+        self.assertTrue(deadlock_node_cycle == [2, 3])
+
+
+class TestTwoThreadsDeadlockGcc(test_tools.MySetUpTestCase):
+    def setUp(self) -> None:
+        self.mySetUp(["main started", "thread started working"], "g++")
+
+    def test_run(self) -> None:
+        error = self.launch_process()
+        self.continue_to_threads_beginning(2)
+        self.continue_process_n_sec(1)
+        has_deadlock, deadlock_cycle = deadlock_detector.find_deadlock(self.debugger)
+        self.assertTrue(has_deadlock)
+        deadlock_node_cycle = [x[0] for x in test_tools.GraphTesting.shift_cycle(deadlock_cycle, 2)]
+        self.assertTrue(deadlock_node_cycle == [2, 3])
+
+
+
+
+
