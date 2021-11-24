864edf8953663a607fee11aaa84e8b2bff18da4f
Artem Zakharenko <zakhart1234567890@gmail.com>
2021-11-24 20:12:45 +0300

    rename wrong recursive mutex name

--- tests/recursive-mutex-test/main.cpp
+++ tests/recursive-mutex-test/main.cpp
@@ -7,48 +7,48 @@ void no_opearation() {
 }
 
 int main() {
-	std::resursive_mutex m1; 
-	std::resursive_mutex m2;
-	std::resursive_mutex m3;
-	std::resursive_mutex m4;
-	std::resursive_mutex m5;
-	std::resursive_mutex m6;
-	std::resursive_mutex m7; 
+	std::recursive_mutex m1; 
+	std::recursive_mutex m2;
+	std::recursive_mutex m3;
+	std::recursive_mutex m4;
+	std::recursive_mutex m5;
+	std::recursive_mutex m6;
+	std::recursive_mutex m7; 
 	no_opearation(); //main started
 	std::thread t1([&]() { 
-		std::unique_lock<std::resursive_mutex> l1(m1); 
+		std::unique_lock<std::recursive_mutex> l1(m1); 
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m2);
+		std::unique_lock<std::recursive_mutex> l2(m2);
 	});
 	std::thread t2([&]() {
-		std::unique_lock<std::resursive_mutex> l1(m2);
+		std::unique_lock<std::recursive_mutex> l1(m2);
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m3);
+		std::unique_lock<std::recursive_mutex> l2(m3);
 	});
 	std::thread t3([&]() {          
-		std::unique_lock<std::resursive_mutex> l1(m3);  
+		std::unique_lock<std::recursive_mutex> l1(m3);  
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m4);
+		std::unique_lock<std::recursive_mutex> l2(m4);
 	});
 	std::thread t4([&]() {          
-		std::unique_lock<std::resursive_mutex> l1(m4);  
+		std::unique_lock<std::recursive_mutex> l1(m4);  
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m5);
+		std::unique_lock<std::recursive_mutex> l2(m5);
 	});
 	std::thread t5([&]() {          
-		std::unique_lock<std::resursive_mutex> l1(m5);  
+		std::unique_lock<std::recursive_mutex> l1(m5);  
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m6);
+		std::unique_lock<std::recursive_mutex> l2(m6);
 	});
 	std::thread t6([&]() {         
-		std::unique_lock<std::resursive_mutex> l1(m6);  
+		std::unique_lock<std::recursive_mutex> l1(m6);  
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m7);
+		std::unique_lock<std::recursive_mutex> l2(m7);
 	});
 	std::thread t7([&]() {          
-		std::unique_lock<std::resursive_mutex> l1(m7); 
+		std::unique_lock<std::recursive_mutex> l1(m7); 
 		no_opearation(); //thread started working
-		std::unique_lock<std::resursive_mutex> l2(m5);
+		std::unique_lock<std::recursive_mutex> l2(m5);
 	});
 	t1.join();
 	t2.join();
