b6959223bfc2d5bd8924bbb0c19826e43df5f4d2
Artem Zakharenko <zakhart1234567890@gmail.com>
2021-11-24 20:41:39 +0300

    Add shared-mutex test, cant find deadlock

---
+++ tests/shared-mutex-test/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+
+void no_opearation() {
+	int x = 0;
+}
+
+int main() {
+	std::shared_mutex m1; 
+	std::shared_mutex m2; 
+	no_opearation(); //main started
+	std::thread t1([&]() {
+		std::unique_lock<std::shared_mutex> l1(m1); 
+		no_opearation(); //thread started working
+		std::shared_lock<std::shared_mutex> l2(m2);
+	});
+	std::thread t2([&]() {
+		std::unique_lock<std::shared_mutex> l1(m2);
+		no_opearation(); //thread started working
+		std::shared_lock<std::shared_mutex> l2(m1);
+	});
+	
+	t1.join();
+	t2.join();
+	return 0;
+}
